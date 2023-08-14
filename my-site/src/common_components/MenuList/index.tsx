e465ede5cf0de56e6f360191e88b755bf181050b
Pawe Pawtel <pawel.pawtel@gmail.com>
2023-08-14 20:43:46 +0200

    14/08

---
+++ my-site/src/common_components/MenuList/index.tsx
@@ -0,0 +1,13 @@
+export const MenuList = () => {
+  return (
+    <div className="menuList">
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
--- my-site/src/components/Footer/index.tsx
+++ my-site/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <div className="pageFooter">
       <div className="leftPart">
         <p>Left icon</p>
-        <p>text more to left</p>
+        <p>planing time of finish page 31/12/2024</p>
       </div>
       <div className="rightPart">right icon</div>
     </div>
